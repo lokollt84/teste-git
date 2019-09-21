c72e1488deb8233b064a6c9d8de68638ba17eb3d
Iberê Amaral <ibere@uol.com.br>
2019-09-21 18:54:28 -0300

    Inicial

---
+++ Course67OlaMundo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Course67OlaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+
+        }
+    }
+}
